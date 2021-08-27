cef6d4a83d05f773d23462deb538e673ed5b4dae
Gobindbatham <Gobindbatham>
2021-08-27 06:51:47 +0530

    Add project files.

---
+++ CustomerCareAPI/Data/CustomerCareAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CustomerCareAPI.Model;
+
+namespace CustomerCareAPI.Data
+{
+    public class CustomerCareAPIContext : DbContext
+    {
+        public CustomerCareAPIContext (DbContextOptions<CustomerCareAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CustomerCareAPI.Model.Message> Message { get; set; }
+    }
+}
---
+++ CustomerCareAPI/Model/ComplaintStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerCareAPI.Model
+{
+    public class ComplaintStatus
+    {
+
+        //Message id
+        public int Id { get; set; }
+
+        //Mesage text
+        public string Status { get; set; }
+
+        
+    }
+}
